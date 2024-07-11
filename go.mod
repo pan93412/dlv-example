419152941bef7074d7b763467f88110682f19573
Yi-Jyun Pan <pan93412@gmail.com>
2024-07-11 19:28:24 +0800

    Initial minimal reproducible environment

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pan93412/dlv-example
+
+go 1.22.1
---
+++ main_test.go
@@ -0,0 +1,12 @@
+package dlvexample_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestExample(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
